f6f2d8e44a6984239213b1c4e6423cefe78edb2c
Vitaly Lukyanov <mszvibel@gmail.com>
2023-03-31 13:11:41 +0300

    Правки

--- HomeWork4/Task24.py
+++ HomeWork4/Task24.py
@@ -7,18 +7,6 @@
 #Напишите программу для нахождения максимального числа ягод, которое может собрать за один заход собирающий модуль, находясь перед 
 #некоторым кустом заданной во входном файле грядки.
 
-#n = int(input())
-#bushes = [int(i) for i in input().split()]
-#bush_max = 0
-
-#for i in range(n):
-#    bush_sum = bushes[i - 1] + bushes[i] + bushes[i + 1 if i < n - 1 else 0]
-#    if bush_sum > bush_max:
-#            bush_max = bush_sum
-
-#print(bush_max)
-
-
 n = 5 # кустов
 from random import randint
 list_1 = list(randint(1, 5) for i in range(int(input('Введите кол-во кустов: '))))
